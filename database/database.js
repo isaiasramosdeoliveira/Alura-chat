02349c3a98ceb39e281774170584a50e0da204b6
Isaías ramos <isaiasramosdeoliverira2003@gmail.com>
2023-02-24 15:20:47 -0300

    Acréscimo do projeto

---
+++ database/database.js
@@ -0,0 +1,24 @@
+const database = [
+    {
+        name: "Isaias",
+        message: "Message"
+    },
+    {
+        name: "Ana",
+        message: "Message"
+    }, {
+        name: "Paulo",
+        message: "Message"
+    }, {
+        name: "Maria",
+        message: "Message"
+    }, {
+        name: "Vanessa",
+        message: "Message"
+    }, {
+        name: "Robert",
+        message: "Message"
+    },
+]
+
+module.exports = database
---
+++ src/components/user.js
@@ -0,0 +1,11 @@
+import database from "../../database/database.js";
+class User{
+    constructor(users){
+        this.users = users
+    }
+    render(){
+        
+    }
+}
+
+const user = new User(database)
